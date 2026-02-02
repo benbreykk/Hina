71835966c7a6e929d2f51c2446586633d013441b
Ben <244203321+benbreykk@users.noreply.github.com>
2026-02-02 20:21:43 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const client = new Client({ intents: [
 // Command handling
 client.commands = new Map();
 const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); 
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
