01a6b967758ef2cdbeaf8811fbec16c231c242cc
Benbreykk <benjamin.breyne@proton.me>
2026-04-08 11:22:20 +0100

    Ajout de la commande /confess qui créer un message embed anonyme pour se confesser en toute discrétion avec la création d'un thread pour empêcher le flood du salon

---
+++ src/commands/fun/confess.js
@@ -0,0 +1,49 @@
+const { Command } = require('@sapphire/framework');
+const { EmbedBuilder, ThreadChannel } = require('discord.js');
+const { CONFESS_CHANNEL_ID } = process.env;
+
+class ConfessCommand extends Command {
+  constructor(context, options) {
+    super(context, { ...options });
+  }
+  // Enregistrer la commande 
+  registerApplicationCommands(registry) {
+    registry.registerChatInputCommand((builder) =>
+      builder.setName('confess')
+        .setDescription('Tu peux faire une confession anonyme dans le serveur')
+        .addStringOption(option => option
+            .setName('message')
+            .setDescription('Le message de ta confession')
+            .setRequired(true)
+        )
+    );
+  }
+
+  async chatInputRun(interaction) {
+    const message = interaction.options.getString('message');
+
+    // Envoyer le message de confession dans un salon spécifique
+    const confessionChannel = interaction.guild.channels.cache.get(CONFESS_CHANNEL_ID);
+    if (!confessionChannel) {
+      return interaction.reply({ content: 'Le salon de confession est introuvable.', ephemeral: true });
+    }
+    const embed = new EmbedBuilder()
+      .setTitle('Nouvelle confession')
+      .setDescription(`${message}`)
+      .setColor('#FF69B4')
+      .setFooter({ text: 'Utilisateur anonyme' })
+      .setTimestamp();
+    const response = await confessionChannel.send({ embeds: [embed] });
+
+    const thead = await confessionChannel.threads.create({
+      name: `Discussion de la confession`,
+      autoArchiveDuration: 60,
+      reason: 'Discussion pour la confession',
+    });
+    thead.send({ content: `Discussion pour la confession : ${message}` });
+    await interaction.reply({ content: 'Ta confession a été envoyée anonymement !', ephemeral: true });
+  }
+}
+module.exports = {
+  ConfessCommand
+};
